62861188a07521a4785f694909c17fe2d7fc53ea
Amado González Rodríguez <amadogr50@gmail.com>
2021-06-23 20:50:40 -0500

    Added styling to navigator

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -2,8 +2,10 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 import TrendingScreen from '../screens/trending-screen'
+import Colors from '../theme/colors'
 import Routes from './routes'
 
 const TrendingStack = createStackNavigator()
@@ -20,15 +22,28 @@ const Tabs = createBottomTabNavigator()
 
 const TabsNavigator = (): JSX.Element => {
   return (
-    <Tabs.Navigator>
-      <Tabs.Screen name={Routes.Trending} component={TrendingStackNavigator} />
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: Colors.text,
+        inactiveTintColor: Colors.notification,
+      }}
+    >
+      <Tabs.Screen
+        name={Routes.Trending}
+        component={TrendingStackNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="star" color={color} size={size} />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   )
 }
 
 const Navigator = (): JSX.Element => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={{ dark: false, colors: Colors }}>
       <TabsNavigator />
     </NavigationContainer>
   )
